3570102c6463eacaa0aa71099f6c02ae16e337f3
Divortio <223161231+divortio@users.noreply.github.com>
2025-10-06 21:25:29 -0400

    i give try

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -41,16 +41,6 @@ find builds -mindepth 1 -maxdepth 1 -type d | while read -r dir; do
   echo "  ✔️  Created individual package: ${dirname}.zip"
 done
 
-## Create a single, all-inclusive zip file.
-#(cd builds && zip -r "../release_assets/ffmpeg-audio-build-all.zip" .)
-#echo "  ✔️  Created all-in-one package: ffmpeg-audio-build-all.zip"
-
-
-# --- 2. Release Tagging ---
-
-# Construct the f
-# --- 3. Verbose Release Notes ---
-
 echo "Generating verbose release notes..."
 
 # Read version numbers from each package.json
@@ -62,30 +52,9 @@ UTIL_VERSION=$(jq -r .version "packages/util/package.json")
 # Get the current ISO 8601 timestamp.
 DATETIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
-
 TAG="v${FFMPEG_VERSION}-${DATETIME}"
 echo "Generated release tag: ${TAG}"
 
-
-# 2. Construct the commit body.
-# Start with a newline to separate it from the title.
-COMMIT_BODY=$'\\n'
-
-# Add the "Packages Built" section.
-COMMIT_BODY+="CI BUILD:${DATETIME}| \\n"
-COMMIT_BODY+="----------------\\n"
-COMMIT_BODY+="- core@${CORE_VERSION}\\n"
-COMMIT_BODY+="- core-mt@${CORE_MT_VERSION}\\n"
-COMMIT_BODY+="- ffmpeg@${FFMPEG_VERSION}\\n"
-COMMIT_BODY+="- util@${UTIL_VERSION}\\n\\n"
-
-# Add the "Build Artifact Paths" section.
-COMMIT_BODY+="Build Artifact Paths:\\n"
-COMMIT_BODY+="---------------------\\n"
-COMMIT_BODY+="- builds/core@${CORE_VERSION}\\n"
-COMMIT_BODY+="- builds/core-mt@${CORE_MT_VERSION}\\n"
-COMMIT_BODY+="- builds/ffmpeg@${FFMPEG_VERSION}\\n"
-COMMIT_BODY+="- builds/util@${UTIL_VERSION}\\n\\n"
 
 # Use a temporary file to store the multi-line release notes.
 RELEASE_NOTES_FILE=$(mktemp)
@@ -93,7 +62,17 @@ RELEASE_NOTES_FILE=$(mktemp)
 # Construct the release body.
 # Using 'cat <<EOF' is a robust way to handle multi-line strings in bash.
 cat <<EOF > "$RELEASE_NOTES_FILE"
-${COMMIT_BODY}
+### Packages Built
+- \`core@${CORE_VERSION}\`
+- \`core-mt@${CORE_MT_VERSION}\`
+- \`ffmpeg@${FFMPEG_VERSION}\`
+- \`util@${UTIL_VERSION}\`
+
+### Build Artifact Paths (in repository)
+- \`builds/core@${CORE_VERSION}\`
+- \`builds/core-mt@${CORE_MT_VERSION}\`
+- \`builds/ffmpeg@${FFMPEG_VERSION}\`
+- \`builds/util@${UTIL_VERSION}\`
 EOF
 
 
